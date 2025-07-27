7b74aabb7d48203a2fa69dc36c57793627a99271
Enrique Delgado <kikeead@gmail.com>
2025-07-27 19:26:30 -0300

    volviendo a bdd local

--- backend/database.py
+++ backend/database.py
@@ -4,8 +4,12 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
+SQLALCHEMY_DATABASE_URL = "sqlite:///./pagos.db"
 load_dotenv()
 
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
